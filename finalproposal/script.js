91a264d1cfdae1e0551d339414662a0f6f0f614c
Ammon Nulu <Ammonnulu@go.byuh.edu>
2025-12-03 21:33:57 -1000

    Final Project

--- finalproposal/script.js
+++ finalproposal/script.js
@@ -1,5 +1,47 @@
-document.getElementById('contactForm').addEventListener('submit', function(e){
-  e.preventDefault(); // prevent page reload
-  document.getElementById('contactForm').reset(); // reset form
-  document.getElementById('successMessage').style.display = 'block'; // show success message
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    document.getElementById('contactForm').reset();
+    document.getElementById('successMessage').style.display = 'block';
+
+    alert("Form Submitted");
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const favoritedish = document.getElementById('dish').value;
+    const recipesuggestions = document.getElementById('suggestion').value;
+    const rating = document.getElementById('rating').value;
+
+    // if (!name || !email) {
+    //     alert("You need a name and email.");
+    //     return;
+    // }
+    
+    const formData = {
+        name: name,
+        email: email,
+        favoritedish: favoritedish,
+        recipesuggestions: recipesuggestions,
+        rating: rating,
+    };
+    console.log(formData);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            alert("Form submitted successfully!");
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+
+            document.getElementById('contactForm').innerHTML = '';
+            // document.getElementById('message').innerText = response.message;
+
+        } else if (xhr.readyState === 4 && xhr.status !== 200) {
+            alert("Error submitting form.");
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+
 });
